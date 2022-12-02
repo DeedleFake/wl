6314cb9b0c6b7e7d612b9c41ef8439b7e3cf06e6
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-02 12:47:13 -0500

    wire: add `SetID()` to `Object`

--- wire/wire.go
+++ wire/wire.go
@@ -97,6 +97,10 @@ type NewID struct {
 
 // Object represents a Wayland protocol object.
 type Object interface {
+	// SetID is used by the object ID tracking system to tell the object
+	// what its own ID should be.
+	SetID(id uint32)
+
 	// Dispatch pertforms the operation requested by the message in the
 	// buffer.
 	Dispatch(msg *MessageBuffer) error
