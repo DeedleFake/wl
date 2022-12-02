6b9f2a2ed433783f1674007983db6eb414e005c6
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-02 14:12:11 -0500

    examples/globals: fix API breakage

--- examples/globals/globals.go
+++ examples/globals/globals.go
@@ -19,12 +19,13 @@ func main() {
 	}
 
 	registry := display.GetRegistry()
-	registry.Global = func(name uint32, inter string, version uint32) {
-		log.Printf("global: name: %v, interface: %q, version: %v", name, inter, version)
-	}
 
 	err = display.RoundTrip()
 	if err != nil {
 		log.Fatalf("round trip: %v", err)
+	}
+
+	for name, inter := range registry.Globals() {
+		log.Printf("global: name: %v, interface: %q, version: %v", name, inter.Name, inter.Version)
 	}
 }
