be20565b86aadffa22a7d1d86a7381ffc043dd36
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-03 21:31:09 -0500

    internal/cq: add

---
+++ internal/cq/cq.go
@@ -0,0 +1,57 @@
+// Package cq implements a simple concurrent queue.
+package cq
+
+import "sync"
+
+type Queue[T any] struct {
+	done  chan struct{}
+	close sync.Once
+
+	add chan T
+	get chan []T
+}
+
+func New[T any]() *Queue[T] {
+	q := Queue[T]{
+		done: make(chan struct{}),
+		add:  make(chan T),
+		get:  make(chan []T),
+	}
+	go q.run()
+
+	return &q
+}
+
+func (q *Queue[T]) Stop() {
+	q.close.Do(func() {
+		close(q.done)
+	})
+}
+
+func (q *Queue[T]) Add() chan<- T {
+	return q.add
+}
+
+func (q *Queue[T]) Get() <-chan []T {
+	return q.get
+}
+
+func (q *Queue[T]) run() {
+	var s []T
+	var get chan []T
+
+	for {
+		select {
+		case <-q.done:
+			return
+
+		case v := <-q.add:
+			s = append(s, v)
+			get = q.get
+
+		case get <- s:
+			s = nil
+			get = nil
+		}
+	}
+}
