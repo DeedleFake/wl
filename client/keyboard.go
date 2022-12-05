92272a04f285a88cc3b4c17a657495dbf325f72e
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-05 14:56:08 -0500

    client: some more implementation of `Keyboard`

--- client/keyboard.go
+++ client/keyboard.go
@@ -4,6 +4,7 @@ import "os"
 
 type Keyboard struct {
 	Keymap     func(format KeyboardKeymapFormat, file *os.File, size uint32)
+	Key        func(serial, time, key uint32, state KeyboardKeyState)
 	RepeatInfo func(rate, delay int32)
 
 	id[keyboardObject]
@@ -17,7 +18,9 @@ type keyboardListener struct {
 func (lis keyboardListener) Keymap(format uint32, fd *os.File, size uint32) {
 	if lis.kb.Keymap != nil {
 		lis.kb.Keymap(KeyboardKeymapFormat(format), fd, size)
+		return
 	}
+	fd.Close()
 }
 
 func (lis keyboardListener) Enter(serial uint32, surface uint32, keys []byte) {
@@ -29,7 +32,9 @@ func (lis keyboardListener) Leave(serial uint32, surface uint32) {
 }
 
 func (lis keyboardListener) Key(serial uint32, time uint32, key uint32, state uint32) {
-	// TODO
+	if lis.kb.Key != nil {
+		lis.kb.Key(serial, time, key, KeyboardKeyState(state))
+	}
 }
 
 func (lis keyboardListener) Modifiers(serial uint32, modsDepressed uint32, modsLatched uint32, modsLocked uint32, group uint32) {
