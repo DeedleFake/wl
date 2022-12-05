faf0e6b2919cdaedf3ccd069417f89c41c2b4b99
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-05 12:17:08 -0500

    client: add `Output`

---
+++ client/output.go
@@ -0,0 +1,59 @@
+package wl
+
+type Output struct {
+	Geometry func(x, y, physicalWidth, physicalHeight, subpixel int32, make, model string, transform OutputTransform)
+	Mode     func(flags OutputMode, width, height, refresh int32)
+	Done     func()
+	Scale    func(factor int32)
+
+	id[outputObject]
+	display *Display
+}
+
+func IsOutput(i Interface) bool {
+	return i.Is(outputInterface, outputVersion)
+}
+
+func BindOutput(display *Display, name uint32) *Output {
+	output := Output{display: display}
+	output.obj.listener = outputListener{output: &output}
+	display.AddObject(&output.obj)
+
+	registry := display.GetRegistry()
+	registry.Bind(name, outputInterface, outputVersion, output.obj.id)
+
+	return &output
+}
+
+func (out *Output) Release() {
+	out.display.Enqueue(out.obj.Release())
+	out.display.DeleteObject(out.obj.id)
+}
+
+type outputListener struct {
+	output *Output
+}
+
+func (lis outputListener) Geometry(x, y, physicalWidth, physicalHeight, subpixel int32, make, model string, transform int32) {
+	if lis.output.Geometry != nil {
+		lis.output.Geometry(x, y, physicalWidth, physicalHeight, subpixel, make, model, OutputTransform(transform))
+	}
+}
+
+func (lis outputListener) Mode(flags uint32, width, height, refresh int32) {
+	if lis.output.Mode != nil {
+		lis.output.Mode(OutputMode(flags), width, height, refresh)
+	}
+}
+
+func (lis outputListener) Done() {
+	if lis.output.Done != nil {
+		lis.output.Done()
+	}
+}
+
+func (lis outputListener) Scale(factor int32) {
+	if lis.output.Scale != nil {
+		lis.output.Scale(factor)
+	}
+}
