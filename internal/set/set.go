b2176ad4e2c2c3d6c1a5b95d053e4ad6e3f0e0f3
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-08 20:10:32 -0500

    internal/set: add `New()`

--- internal/set/set.go
+++ internal/set/set.go
@@ -2,6 +2,14 @@ package set
 
 type Set[T comparable] map[T]struct{}
 
+func New[T comparable](vals ...T) Set[T] {
+	s := make(Set[T], len(vals))
+	for _, v := range vals {
+		s.Add(v)
+	}
+	return s
+}
+
 func (s Set[T]) Add(v T) {
 	s[v] = struct{}{}
 }
