6ac99ac389ae52341200328c6aa37bcb98428eed
DeedleFake <deedlefake@users.noreply.github.com>
2022-12-13 23:47:33 -0500

    server: add an `objstore.Store` to `Client`

--- server/client.go
+++ server/client.go
@@ -1,11 +1,15 @@
 package wl
 
-import "deedles.dev/wl/wire"
+import (
+	"deedles.dev/wl/internal/objstore"
+	"deedles.dev/wl/wire"
+)
 
 type Client struct {
 	server *Server
 	done   chan struct{}
 	conn   *wire.Conn
+	store  *objstore.Store
 }
 
 func newClient(server *Server, conn *wire.Conn) *Client {
@@ -13,6 +17,7 @@ func newClient(server *Server, conn *wire.Conn) *Client {
 		server: server,
 		done:   make(chan struct{}),
 		conn:   conn,
+		store:  objstore.New(1 << 24),
 	}
 	go client.listen()
 
